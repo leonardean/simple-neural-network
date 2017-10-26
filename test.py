4b6d7e3967e1c0a3d9f8e297f4ef420c1beb4348
Zhanwen xu <leon.xu@kii.com>
2017-10-26 16:24:20 +0800

    removed test file

--- test.py
+++
@@ -1,9 +0,0 @@
-import DataProcessor as dp
-
-data_processor = dp.DataProcessor('Bike-Sharing-Dataset/hour.csv')
-# data_processor.show_data()
-data_processor.virtualize()
-# data_processor.show_data()
-data_processor.normalize()
-data_processor.split()
-a,b,c,d,e,f = data_processor.get_data()
